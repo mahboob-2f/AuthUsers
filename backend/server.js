0ee540a9a38294771a363d0af2ebe66491772c75
mahboob alam <mahboob6550@gmail.com>
2025-11-20 22:26:54 +0530

    adding some parsers and creating express app

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,24 @@
-import express from 'express'
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config({path:'./.env',});
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(cors({credentials:true}))
+
+
+const port = process.env.PORT || 4001;
+  
 app.get('/',(req,res)=>{
     res.send("hello ghost");
 });
 
-app.listen(4000,()=>{
-    console.log("server is listening at port",4000);
+app.listen(port,()=>{
+    console.log("server is listening at port",port);
 })
