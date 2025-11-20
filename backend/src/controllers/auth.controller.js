591baf15fd713624a53c66ac5561ba96d51eb9ee
mahboob alam <mahboob6550@gmail.com>
2025-11-21 02:10:04 +0530

    working with the  register controller function

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,78 @@
+import validator from 'validator';
+import { User } from '../models/user.models';
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken';
+
+
+export const register = async(req,res)=>{
+    //  for registering 
+    //    get details from request body
+    //      check and validate that details like name, and email
+    //     check for the user is already exist or not
+    //      database call create user
+    //
+
+
+    try {
+        const {name,email,password}= req.body;
+        if(
+            [name,email,password].some((field)=> field?.trim()==='')
+        ){
+            return res.status(500)
+                .json({
+                    success:false,
+                    message:"Required field are missing",
+                })
+        }
+        //   validating email               we can use regular expressions also
+        //                          but this validator package is better
+
+        if(!validator.isEmail({email})){
+            return res.status(400)
+                .json({
+                    success:false,
+                    message:"Email is not valid"
+                })
+        }
+
+
+        //    checking whether user already exists or not
+
+        const existedUser = await User.findOne({email});
+        if(existedUser){
+            return res.status(409)
+                .json({
+                    success:false,
+                    message:"User already exists.",
+                })
+        }
+
+        //   hashing the password   => we can use code to manually hashed the password
+
+        const hashedPassword = await bcrypt.hash(password,10);   
+
+        //      creating user and saving in database using create 
+
+        const user = await User.create({
+            name,
+            email,
+            password:hashedPassword
+        })
+
+
+        //     here to start ,  generating tokens  
+
+
+
+        
+        
+    } catch (error) {
+        console.log("error while registering the user",error.message);
+        return res.status(500)
+            .json({
+                success:false,
+                message:`user is not registered ${error.message}`,
+            })
+    }   
+
+}
--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcryptjs";
 import mongoose from "mongoose";  
 import { Schema } from "mongoose"; 
 
@@ -36,5 +37,23 @@ const userSchema = new Schema({
         default:0
     }
 },{});
+
+//         these two methods are very useful , they are doing the same things
+//               like hashing the password 
+
+// userSchema.pre("save",async(next)=>{
+//     if(this.isModified("password")){
+//         this.password = await bcrypt.hash(this.password,10);
+//         next();
+//     }else{
+//         next();
+//     }
+// })
+
+// userSchema.methods.setPassword = async(password)=>{
+//     this.password = await bcrypt.hash(password,10);
+// }
+
+
 
 export const User =mongoose.models.User || mongoose.model("User",userSchema);
