ca801140d9f4bd36b6d1207d5d3074e6c6aa81ba
mahboob alam <mahboob6550@gmail.com>
2025-12-03 00:00:14 +0530

    userAuth middleware  added  for finding userId

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,40 @@
+
+import jwt from 'jsonwebtoken'
+import User from '../models/user.models.js'
+
+
+const userAuth = async(req,res,next)=>{
+    try{
+        
+        const {token} = req.cookies;
+        if(!token){
+            return res.status(400)
+                .json({
+                    success:false,
+                    message:'Unauthorised User',
+                })
+        }
+
+        const decodedToken = jwt.verify(token,process.env.SECRETKEY);
+        
+        if(decodedToken?.id){
+            req.body.userId= decodedToken.id;
+        }else{
+            return res.status(400)
+                .json({
+                    success:false,
+                    message:"Unauthorised user",
+                })
+        }
+        next();
+
+    }catch(error){
+        return res.status(400)
+            .json({
+                success:false,
+                message:`Invalid cookie ${error.message}`,
+            })
+    }
+}
+
+export {userAuth};
