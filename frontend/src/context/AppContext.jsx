abc15b2b4fd7a4dc4fde233ba6af6aedf8ace300
mahboob alam <mahboob6550@gmail.com>
2025-12-13 23:03:53 +0530

    App Context created successfully

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -3,10 +3,14 @@ import { createContext } from "react";
 
 export const AppContext = createContext();
 
-export const AppContextProvider=(props)=>{
-    
+export const AppContextProvider = (props) => {
+
+    const value={
+
+    }
+
     return (
-        <AppContext.Provider  >
+        <AppContext.Provider value={value} >
             {props.children}
         </AppContext.Provider>
     )
